4825ce8d140d57f37d1eb7d317c0aff5ff792a85
Honor Everhart <honoreverhart@gmail.com>
2025-05-07 17:42:44 -0600

    each component file

---
+++ tsc/src/components/about.jsx
@@ -0,0 +1,17 @@
+export default function About(){
+    return(
+        <>
+        <div>
+            <h1>About Us</h1>
+            <div>
+                <h2>Mission</h2>
+                <p>blah blah blah</p>
+                <h2>Time</h2>
+                <p>blah blah blah</p>
+                <h2>Place</h2>
+                <p>blah blah blah</p>
+            </div>
+        </div>
+        </>
+    )
+}
---
+++ tsc/src/components/homePage.jsx
@@ -0,0 +1,20 @@
+export default function HomePage() {
+
+  return(
+    <>
+        <div className="navbar">
+            <div className="logo"></div>
+            <button>About</button>
+            <button>Connect</button>
+            <button>Events</button>
+            <button>Give</button>
+        </div>
+
+        <div className="homepage">
+            <h1>The Severance Connection</h1>
+            <p>description blah blah blah</p>
+            <button>Join Us!</button>
+        </div>
+    </>
+  );
+}
